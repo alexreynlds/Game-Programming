58c74c424d292854f2ba914fd803060cf824d5ec
Alex Reynolds <ajrey@live.co.uk>
2022-03-14 14:45:15 +0000

    First Commit
    - Added .gitignore
    - Added SDL
    - Creating a window using SDL

--- Game Programming Assessment/Game Programming Assessment.cpp
+++ Game Programming Assessment/Game Programming Assessment.cpp
@@ -2,19 +2,22 @@
 //
 
 #include <iostream>
+#include "SDL.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//int main()
+//{
+//    std::cout << "Hello World!\n";
+//}
+
+//Creating a window using SDL
+int main(int argc, char  *argv[]) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		return 0;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Test", 250, 250, 800, 600, SDL_WINDOW_SHOWN);
+
+	SDL_Delay(4000);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
